6cdc82b126c330440770cf9d172d347fdf20f9ef
derdeche <foued.derdeche@gmail.com>
2022-12-09 16:31:22 +0100

    creation function construct

--- film.php
+++ film.php
@@ -9,3 +9,12 @@ class film
     private string $_genre;
 
 }
+
+public function __construct($titrefilm, $datesortie, $duree, $unirealisateur, $genre)
+{
+    $this->_titrefilm = $titrefilm;
+    $this->_datesortie = $datesortie;
+    $this->_duree = $duree;
+    $this->_unirealisateur = $unirealisateur;
+    $this->unirealisateur->addFilm($this);
+}
