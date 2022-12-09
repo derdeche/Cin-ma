ac0ade61338a75dae8010ddf619b4fc7e5e1b85d
derdeche <foued.derdeche@gmail.com>
2022-12-09 16:40:20 +0100

    get and set of class film

--- film.php
+++ film.php
@@ -18,3 +18,52 @@ public function __construct($titrefilm, $datesortie, $duree, $unirealisateur, $g
     $this->_unirealisateur = $unirealisateur;
     $this->unirealisateur->addFilm($this);
 }
+
+public function getTitrefilm()
+{
+    return $this->_titrefilm;
+}
+
+public function setTitrefilm($titrefilm)
+{
+    $this->_titrefilm = $titrefilm;
+    return $this;
+}
+
+public function getDatesortie()
+{
+    return $this->_datesortie;
+}
+
+public function setDatesortie($datesortie)
+{
+    $this->_datesortie = $datesortie;
+    return $this;
+}
+
+public function getDuree()
+{
+    return $this->_Duree;
+}
+
+public function setDuree($duree)
+{
+    $this->_duree = $duree;
+    return $this;
+}
+
+public function getUnirealisateur()
+{
+    return $this->_unirealisateur;
+}
+
+public function setUnirealisateur($unirealisateur)
+{
+    $this->_unirealisateur = $unirealisateur;
+    return $this;
+}
+
+public function __tostring()
+{
+    return  $this->_titrefilm;
+}
