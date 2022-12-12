94b3b3649c8a79717b955f3dfd9239d93be39287
derdeche <foued.derdeche@gmail.com>
2022-12-12 14:17:20 +0100

    extends personne

--- Réalisateur.php
+++ Réalisateur.php
@@ -1,71 +1,23 @@
 <?php
 
-class Réalisateur 
+class Réalisateur extends Personne
 {
-    private $_nom;
-    private string $_prenom;
-    private string $_sexe;
-    private int $_datenaissance;
-    private array $_titrefilm;
+
+    private array $_films;
 
 
-public function __construct($nom, $prenom, $sexe, $datenaissance, $titrefilm)
+public function __construct($nom, $prenom, $sexe, $datenaissance)
 {
-    $this->_nom = $nom;
-    $this->_prenom = $prenom;
-    $this->_datenaissance = $datenaissance;
-    $this->_titrefilm = [];
+    parent::__construct($nom, $prenom, $sexe, $datenaissance) ;
+
+    $this->_films = [];
 }
 
-public function addTitrefilm($titrefilm)
+public function addTitrefilm($film)
 {   
  
-    $this->_titrefilm[]= $titrefilm;
+    $this->_films[]= $film;
 } 
-
-public function getNom()
-{
-    return $this->_nom;
-}
-
-public function setNom($nom)
-{
-    $this->_nom = $nom;
-    return $this;
-}
-
-public function getPrenom()
-{
-    return $this->_prenom;
-}
-
-public function setPrenom($prenom)
-{
-    $this->_prenom = $prenom;
-    return $this;
-}
-
-public function getSexe()
-{
-    return $this->_sexe;
-}
-
-public function setSexe($sexe)
-{
-    $this->_sexe = $sexe;
-    return $this;
-}
-
-public function getDatenaissance()
-{
-    return $this->_datenaissance;
-}
-
-public function setDatenaissance($datenaissance)
-{
-    $this->_datenaissance = $datenaissance;
-    return $this;
-}
 
 
 
