ac714299a756fb8202ef7d78703f9c2f1e01655d
derdeche <foued.derdeche@gmail.com>
2022-12-12 11:43:20 +0100

    rajout fonction construct

--- Genre.php
+++ Genre.php
@@ -1,6 +1,40 @@
 <?php
 class genre 
 {
-    private $_titrefilm;
+    private $_action;
+    private $_adventure;
+    private $_sciencefiction;
+    private $_thriller;
+
+
+public function __construct( $action, $adventure, $sciencefiction, $thriller)
+{
+    $this->_action = $action;
+    $this->_adventure = $adventure;
+    $this->_sciencefiction = $sciencefiction;
+    $this->_thriller = $thriller;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
