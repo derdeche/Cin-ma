7613eb33de9a89dfb69738de49e0228520239280
derdeche <foued.derdeche@gmail.com>
2022-12-12 11:58:24 +0100

    set and get class personne

--- personne.php
+++ personne.php
@@ -6,4 +6,63 @@ class personne
     private string $_prenom;
     private string $_sexe;
     private int $_datenaissance;
+
+
+public function getNom()
+{
+    return $this->_nom;
 }
+
+public function setNom($nom)
+{
+    $this->_nom = $nom;
+    return $this;
+}
+
+public function getPrenom()
+{
+    return $this->_prenom;
+}
+
+public function setPrenom($prenom)
+{
+    $this->_prenom = $pernom;
+    return $this;
+}
+
+public function getSexe()
+{
+    return $this->_sexe;
+}
+
+public function setSexe($sexe)
+{
+    $this->_sexe = $sexe;
+    return $this;
+}
+
+public function getDatenaissance()
+{
+    return $this->_datenaissance;
+}
+
+public function setDatenaissance($datenaissance)
+{
+    $this->_datenaissance = $datenaissance;
+    return $this;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
