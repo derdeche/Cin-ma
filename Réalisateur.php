251d6c6a4f769ade975ae6eacc89795996b2cce7
derdeche <foued.derdeche@gmail.com>
2022-12-12 10:56:22 +0100

    get and set

--- Réalisateur.php
+++ Réalisateur.php
@@ -7,7 +7,7 @@ class Réalisateur
     private string $_sexe;
     private int $_datenaissance;
     private array $_titrefilm;
-}
+
 
 public function __construct($nom, $sexe, $datenaissance, $titrefilm)
 {
@@ -17,3 +17,73 @@ public function __construct($nom, $sexe, $datenaissance, $titrefilm)
     $this->_titrefilm = [];
 }
 
+public function getNom()
+{
+    return $this->_nom;
+}
+
+public function setNom($nom)
+{
+    $this->_nom = $nom;
+    return $this;
+}
+
+public function getPrenom()
+{
+    return $this->_prenom;
+}
+
+public function setPrenom($prenom)
+{
+    $this->_prenom = $prenom;
+    return $this;
+}
+
+public function getSexe()
+{
+    return $this->_sexe;
+}
+
+public function setSexe($sexe)
+{
+    $this->_sexe = $sexe;
+    return $this;
+}
+
+public function getDatenaissance()
+{
+    return $this->_datenaissance;
+}
+
+public function setDatenaissance($datenaissance)
+{
+    $this->_datenaissance = $datenaissance;
+    return $this;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
