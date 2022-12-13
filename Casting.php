93e7d51c8e1264ec6df32307007d7b7826b67153
derdeche <foued.derdeche@gmail.com>
2022-12-13 14:58:58 +0100

    add get and set

--- Casting.php
+++ Casting.php
@@ -1,20 +1,62 @@
 <?php
 class casting
 {
-    private $_film;
-    private $_role;
-    private $_acteur;
+    private role $_role;
+    private acteur $_acteur;
+    private array $_film;
+    
 
-
-public function __construct($film, $role, $acteur)
+public function __construct($role, $acteur)
 {
-    $this->_film = $film;
     $this->_role = $role;
     $this->_acteur = $acteur;
+    $this->_film = [];
+    
 }
+
+public function addFilm($film)
+{   
+ 
+    $this->_films[]= $film;
+} 
+
 
 public function getFilm()
 {
     return $this->_film;
 }
+
+public function setFilm($film)
+{
+    $this->_film = $film;
+    return $this;
+}
+
+public function getRole()
+{
+    return $this->_role;
+}
+
+public function setRole()
+{
+    $this->_role = $role;
+    return $this;
+}
+public function getActeur()
+{
+    return $this->_acteur;
+}
+
+public function setActeur($acteur)
+{
+    $this->_acteur = $acteur;
+    return $this;
+}
+
+
+
+
+
+
+
 }
