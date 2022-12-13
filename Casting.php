1da514ca9dceb86b84c874edfa1c0f38d1cb9a53
derdeche <foued.derdeche@gmail.com>
2022-12-13 11:51:50 +0100

    rajout construct

--- Casting.php
+++ Casting.php
@@ -4,5 +4,13 @@ class casting
     private $_film;
     private $_role;
     private $_acteur;
+
+
+public function __construct($film, $role, $acteur)
+{
+    $this->_film = $film;
+    $this->_role = $role;
+    $this->_acteur = $acteur;
 }
 
+}
