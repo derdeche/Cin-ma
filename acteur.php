82d4ed80bfab1557da4e0424cbc9034743f309ef
derdeche <foued.derdeche@gmail.com>
2022-12-13 15:01:47 +0100

    création

---
+++ acteur.php
@@ -0,0 +1,20 @@
+<?php
+class acteur extends Personne
+{
+
+    private array $_films;
+
+
+public function __construct($nom,$prenom, $sexe, $datenaissance)
+{
+    parent::__construct($nom,$prenom, $sexe, $datenaissance);
+
+    $this->_films = [];
+}
+
+public function addFilm($film)
+{   
+ 
+    $this->_films[]= $film;
+} 
+}
