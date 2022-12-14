1ac8ec617b3d6d06622878aee6a48b346f22ea5a
derdeche <foued.derdeche@gmail.com>
2022-12-14 14:51:18 +0100

    rajout toString, set et get

--- role.php
+++ role.php
@@ -4,5 +4,61 @@ class role
 {
     private $_role;
     private array $_casting;
+
+public function __construct($role)
+{
+    $this->_role = $role;
+    $this->_casting = [];
+}
+
+public function addCasting($casting)
+{
+    $this->_casting[] = $casting;
+    return $this;
+}
+
+public function getRole()
+{
+    return $this->_role;
+}
+
+public function setRole()
+{
+    $this->_casting = $casting;
+    return $this;    
+}
+
+public function __toString()
+{
+    return "Le role joué est ".$this->getRole();
+}
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
