778cd78505ed30745456e735df660c3d911edb61
derdeche <foued.derdeche@gmail.com>
2022-12-15 09:41:44 +0100

    rajout function afficherCasting

--- role.php
+++ role.php
@@ -21,17 +21,26 @@ public function getRole()
 {
     return $this->_role;
 }
+ 
 
-public function setRole()
-{
-    $this->_casting = $casting;
-    return $this;    
-}
 
 public function __toString()
 {
     return "Le role joué est ".$this->getRole();
 }
+
+public function afficherCasting()
+{
+    echo 'l acteur' .$this. 'a  joué le role de'. $this->getRole();
+    foreach($this->_casting as $casting)
+    {
+        echo $casting."<br>";
+    }
+ 
+    
+  
+
+
 
 
 
