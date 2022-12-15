0442389af1d514e4d193174f54c7a5071010e247
derdeche <foued.derdeche@gmail.com>
2022-12-15 11:03:55 +0100

    supp set

--- film.php
+++ film.php
@@ -10,7 +10,7 @@ class film
     private array $_casting;
 
     
-    public function __construct($titrefilm, $datesortie, $duree, $unirealisateur, $genre, $casting)
+    public function __construct($titrefilm, $datesortie, $duree, $unirealisateur, $genre)
     {
         $this->_titrefilm = $titrefilm;
         $this->_datesortie = $datesortie;
@@ -20,7 +20,7 @@ class film
         $this->_casting = [];
         $this->_unirealisateur->addFilm($this);
         $this->_genre->addFilm($this);
-        $this->_casting->addFilm($this);
+        
     }
     
     public function addCasting($casting)
@@ -34,62 +34,39 @@ class film
         return $this->_titrefilm;
 }
 
-public function setTitrefilm($titrefilm)
-{
-    $this->_titrefilm = $titrefilm;
-    return $this;
-}
+
 
 public function getDatesortie()
 {
     return $this->_datesortie;
 }
 
-public function setDatesortie($datesortie)
-{
-    $this->_datesortie = $datesortie;
-    return $this;
-}
 
 public function getDuree()
 {
     return $this->_duree;
 }
 
-public function setDuree($duree)
-{
-    $this->_duree = $duree;
-    return $this;
-}
 
 public function getUnirealisateur()
 {
     return $this->_unirealisateur;
 }
 
-public function setUnirealisateur($unirealisateur)
-{
-    $this->_unirealisateur = $unirealisateur;
-    return $this;
-}
 
 public function getGenre()
 {
     return $this->_genre;
 }
 
-public function setGenre($genre)
-{
-    $this->_genre = $genre;
-    return $this;
-}
+
 
 public function __tostring()
 {
     return "Titre du film : ". $this->_titrefilm ."<br>". "Date de sortie :  ". $this->_datesortie."<br>"." Durée :  " .$this->getDuree()."min"."<br>" .$this->getUnirealisateur();
 }
 
-public function afficherinfo()
+public function afficherInfo()
 {
     echo $this;
     foreach ($this->_casting as $casting)
