136adcc3e23157646848d3c88aad69ff20aa1e27
derdeche <foued.derdeche@gmail.com>
2022-12-27 00:05:08 +0100

    rajout commentaire

--- film.php
+++ film.php
@@ -1,6 +1,6 @@
 <?php
 
-class film
+class Film
 {
     private string $_titrefilm;
     private string $_datesortie;
@@ -10,7 +10,7 @@ class film
     private array $_casting;
 
     
-    public function __construct($titrefilm, $datesortie, $duree, $unirealisateur, $genre, $casting)
+    public function __construct($titrefilm, $datesortie, $duree, $unirealisateur, $genre)
     {
         $this->_titrefilm = $titrefilm;
         $this->_datesortie = $datesortie;
@@ -20,7 +20,7 @@ class film
         $this->_casting = [];
         $this->_unirealisateur->addFilm($this);
         $this->_genre->addFilm($this);
-        $this->_casting->addFilm($this);
+        
     }
     
     public function addCasting($casting)
@@ -34,60 +34,62 @@ class film
         return $this->_titrefilm;
 }
 
-public function setTitrefilm($titrefilm)
-{
-    $this->_titrefilm = $titrefilm;
-    return $this;
-}
+
 
 public function getDatesortie()
 {
     return $this->_datesortie;
 }
 
-public function setDatesortie($datesortie)
-{
-    $this->_datesortie = $datesortie;
-    return $this;
-}
 
 public function getDuree()
 {
     return $this->_duree;
 }
 
-public function setDuree($duree)
-{
-    $this->_duree = $duree;
-    return $this;
-}
 
 public function getUnirealisateur()
 {
     return $this->_unirealisateur;
 }
 
-public function setUnirealisateur($unirealisateur)
-{
-    $this->_unirealisateur = $unirealisateur;
-    return $this;
-}
 
 public function getGenre()
 {
     return $this->_genre;
 }
 
-public function setGenre($genre)
-{
-    $this->_genre = $genre;
-    return $this;
-}
-
-public function __tostring()
+                       /* Affichage info film*/
+public function getInfofilm()
 {
     return "Titre du film : ". $this->_titrefilm ."<br>". "Date de sortie :  ". $this->_datesortie."<br>"." Durée :  " .$this->getDuree()."min"."<br>" .$this->getUnirealisateur();
 }
+
+
+public function __tostring()
+{
+    return  $this->_titrefilm ."<br>";
+
+}
+/*public function afficherInfo()
+{
+    echo $this;
+    foreach ($this->_casting as $casting)
+{
+        echo $casting."<br>";
+    
+}*/
+
+    
+
+
+    
+       
+       
+       
+       
+    
+
 
 
 
