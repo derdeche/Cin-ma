dc1273dccf877da7cc2a386b0a9adb3a4e7e4929
derdeche <foued.derdeche@gmail.com>
2022-12-27 00:03:58 +0100

    rajout  commentaire

--- Casting.php
+++ Casting.php
@@ -1,25 +1,20 @@
 <?php
-class casting
+class Casting
 {
     private role $_role;
     private acteur $_acteur;
-    private array $_film;
+    private film $_film;
     
 
-public function __construct($role, $acteur)
+public function __construct($role, $acteur,$film)
 {
     $this->_role = $role;
     $this->_acteur = $acteur;
-    $this->_film = [];
-    $this->_acteur->addFilm($this);
-    $this->_role->addFilm($this);    
+    $this->_film = $film;
+    $this->_film->addCasting($this);
+    $this->_acteur->addCasting($this);
+    $this->_role->addCasting($this);    
 }
-
-public function addFilm($film)
-{   
- 
-    $this->_films[]= $film;
-} 
 
 
 public function getFilm()
@@ -27,40 +22,29 @@ public function getFilm()
     return $this->_film;
 }
 
-public function setFilm($film)
-{
-    $this->_film = $film;
-    return $this;
-}
-
 public function getRole()
 {
     return $this->_role;
 }
 
-public function setRole()
-{
-    $this->_role = $role;
-    return $this;
-}
+
 public function getActeur()
 {
     return $this->_acteur;
 }
 
-public function setActeur($acteur)
-{
-    $this->_acteur = $acteur;
-    return $this;
-}
 
-public function __toString()
+
+/*public function __toString()
 {
     return "L'acteur". $this->getActeur() . "a joué le role de " . $this->getRole() ."dans". $this->getFilm();
+}*/
+                 /* Affichage casting Acteur*/
+public function getCasting()
+{
+    echo "Dans le film".$this->getFilm()."L'acteur". $this->getActeur() . "a joué le role de " . $this->getRole() ."<br>";
 }
 
 
-
-
-
 }
+
