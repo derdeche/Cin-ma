25a1105a1eef1dd2b07b658105082e98dec4c225
derdeche <foued.derdeche@gmail.com>
2022-12-27 00:11:35 +0100

    rajout commentaire

--- role.php
+++ role.php
@@ -1,6 +1,6 @@
 <?php
 
-class role 
+class Role 
 {
     private $_role;
     private array $_casting;
@@ -22,43 +22,35 @@ public function getRole()
     return $this->_role;
 }
 
-public function setRole()
-{
-    $this->_casting = $casting;
-    return $this;    
-}
 
 public function __toString()
 {
-    return "Le role joué est ".$this->getRole();
+    return $this->getRole();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                 /* Affichage des Roles joués par l'acteur*/
+public function afficherCasting()
+{
+    echo 'l acteur' .$this. 'a  joué le role de'. $this->getRole();
+    foreach($this->_casting as $casting)
+    {
+        echo $casting."<br>";
+    }
 }
 
+                 /*Affichage des Acteurs qui ont joués tels Role*/
+public function afficherRole(){
+
+    echo "<br> Le role  ".$this. " a été joué par :<br>";
+
+    foreach($this->_casting as $casting)
+    {
+
+        echo $casting->getActeur()."<br>";
+
+    }
+
+        
+
+} 
+}
